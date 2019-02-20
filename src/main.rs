20f0fd874155135961243bc6766183adbeea0360
ZapAnton <wc3ft2@gmail.com>
2019-02-20 18:15:19 +0300

    Implemented simple PPM output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{self, BufWriter, Write},
+};
+
+fn main() -> io::Result<()> {
+    let nx = 200;
+
+    let ny = 100;
+
+    let mut output_file = BufWriter::new(File::create("test.ppm")?);
+
+    write!(&mut output_file, "P3\n{} {}\n255\n", nx, ny)?;
+
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let r = i as f32 / nx as f32;
+
+            let g = j as f32 / ny as f32;
+
+            let b = 0.2;
+
+            let ir = (255.99 * r) as i32;
+
+            let ig = (255.99 * g) as i32;
+
+            let ib = (255.99 * b) as i32;
+
+            write!(&mut output_file, "{} {} {}\n", ir, ig, ib)?;
+        }
+    }
+
+    Ok(())
 }
