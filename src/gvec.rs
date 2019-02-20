022d6dcd201429d781d9bd9f481d022d8a4fb78e
ZapAnton <wc3ft2@gmail.com>
2019-02-21 00:24:48 +0300

    Implemented Add, Mul<f32> for Gvec and Mul<Gvec> for f32

--- src/gvec.rs
+++ src/gvec.rs
@@ -1,17 +1,32 @@
-/*pub struct Gvec {
-    pub e: [f32; 3],
-}
-
-impl Gvec {
-    pub fn new(e0: f32, e1: f32, e2: f32) -> Self {
-        Self { e: [e0, e1, e2] }
-    }
-}*/
-
+use std::ops::{Add, Mul};
 pub struct Gvec(pub f32, pub f32, pub f32);
 
 impl Gvec {
     pub fn new(e0: f32, e1: f32, e2: f32) -> Self {
         Self(e0, e1, e2)
+    }
+}
+
+impl Add for Gvec {
+    type Output = Gvec;
+
+    fn add(self, other: Self) -> Self::Output {
+        Gvec(self.0 + other.0, self.1 + other.1, self.2 + other.2)
+    }
+}
+
+impl Mul<f32> for Gvec {
+    type Output = Gvec;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self(self.0 * rhs, self.1 * rhs, self.2 * rhs)
+    }
+}
+
+impl Mul<Gvec> for f32 {
+    type Output = Gvec;
+
+    fn mul(self, rhs: Gvec) -> Self::Output {
+        Gvec(self * rhs.0, self * rhs.1, self * rhs.2)
     }
 }
