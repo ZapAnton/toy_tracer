46bb5c673f2a30dba7bd799d46ccc67cc3171be2
ZapAnton <wc3ft2@gmail.com>
2019-03-11 17:01:07 +0300

    Remade the project to follow another tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,38 @@
 use std::{
-    borrow::Borrow,
     fs::File,
     io::{self, BufWriter, Write},
 };
 
-use toy_tracer::{camera::Camera, gvec::Gvec, ray::Ray, sphere::Sphere, Hitable};
-
-use rand::{self, Rng};
+use toy_tracer::gvec::Gvec;
 
 const OUTPUT_FILE_PATH: &str = "out/test.ppm";
 
-const NX: i32 = 200;
-const NY: i32 = 100;
-const NS: i32 = 100;
-
-fn color<T: Hitable>(ray: &Ray, world: &[T], depth: i32) -> Gvec {
-    let (hit_record, _) = world.iter().fold(
-        (Option::default(), std::f32::MAX),
-        |(hit_record, closest), item| {
-            if let Some(temp_record) = item.hit(ray, 0.001, closest) {
-                let closest = temp_record.t;
-
-                (Some(temp_record), closest)
-            } else {
-                (hit_record, closest)
-            }
-        },
-    );
-
-    if let Some(hit_record) = hit_record {
-        /*
-        let target: Gvec = hit_record.p.borrow() as &Gvec
-            + hit_record.normal.borrow()
-            + toy_tracer::unit_sphere_random_point();
-
-        let direction = target - hit_record.p.borrow();
-
-        let ray = Ray::new(&hit_record.p, &direction);
-
-        0.5 * color(&ray, world)
-        */
-        if let Some(scattered_ray) = hit_record.material.scatter(ray, hit_record) && depth < 50 {
-            scattered_ray.attenuation * color(scattered_ray.ray, world, depth + 1)
-        } else {
-            Gvec(0.0, 0.0, 0.0)
-        }
-    } else {
-        let unit_direction = ray.direction.unit();
-
-        let t = 0.5 * (unit_direction.1 + 1.0);
-
-        (1.0 - t) * Gvec(1.0, 1.0, 1.0) + t * Gvec(0.5, 0.7, 1.0)
-    }
-}
+const WIDTH: i32 = 1024;
+const HEIGHT: i32 = 768;
 
 fn main() -> io::Result<()> {
     let mut output_file = BufWriter::new(File::create(OUTPUT_FILE_PATH)?);
 
-    write!(&mut output_file, "P3\n{} {}\n255\n", NX, NY)?;
+    writeln!(&mut output_file, "P3\n{} {}\n255", WIDTH, HEIGHT)?;
 
-    let world = [
-        Sphere::new(0.5, &Gvec(0.0, 0.0, -1.0)),
-        Sphere::new(100.0, &Gvec(0.0, -100.5, -1.0)),
-    ];
-
-    let camera = Camera::default();
-
-    let mut rng = rand::thread_rng();
-
-    for j in (0..NY).rev() {
-        for i in 0..NX {
-            let col: Gvec = (0..NS)
-                .map(|_| {
-                    let u = (i as f32 + rng.gen::<f32>()) / NX as f32;
-
-                    let v = (j as f32 + rng.gen::<f32>()) / NY as f32;
-
-                    let ray_direction = camera.lower_left_corner - camera.origin
-                        + u * camera.horizontal
-                        + v * camera.vertical;
-
-                    let ray = Ray::new(camera.origin, &ray_direction);
-
-                    color(&ray, &world)
-                })
-                .fold(Gvec(0.0, 0.0, 0.0), |sum, color| sum + color)
-                / (NS as f32);
-
-            let ir = (255.99 * col.0.sqrt()) as u32;
-
-            let ig = (255.99 * col.1.sqrt()) as u32;
-
-            let ib = (255.99 * col.2.sqrt()) as u32;
-
-            writeln!(&mut output_file, "{} {} {}", ir, ig, ib)?;
-        }
-    }
+    (0..HEIGHT)
+        .flat_map(|height| {
+            (0..WIDTH).map(move |width| {
+                Gvec(
+                    height as f32 / HEIGHT as f32,
+                    width as f32 / WIDTH as f32,
+                    0.0,
+                )
+            })
+        })
+        .map(|frame| {
+            (
+                (255.0 * f32::max(0.0, f32::min(1.0, frame.0))) as u32,
+                (255.0 * f32::max(0.0, f32::min(1.0, frame.1))) as u32,
+                (255.0 * f32::max(0.0, f32::min(1.0, frame.2))) as u32,
+            )
+        })
+        .try_for_each(|(ir, ig, ib)| writeln!(&mut output_file, "{} {} {}", ir, ig, ib))?;
 
     Ok(())
 }
